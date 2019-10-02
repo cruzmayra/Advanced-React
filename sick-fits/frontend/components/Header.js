a68e15dc4c5c9ab0d3470a3b2d6dbe5bd4edc45b
Mayra Cruz <mcruz@iMac-de-Administrador.local>
2019-10-02 16:47:31 -0500

    Video4 Custom _app.js

---
+++ sick-fits/frontend/components/Header.js
@@ -0,0 +1,16 @@
+import Nav from './Nav'
+
+const Header = () => (
+  <div>
+    <div className="bar">
+      <a href="">Sick Fits</a>
+      <Nav />
+    </div>
+    <div className="sub-bar">
+      <p>Search</p>
+    </div>
+    <di>Cart</di>
+  </div>
+)
+
+export default Header
